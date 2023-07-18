b0a72a9a47d82f39bc4ebcdbd9641622ce09dc3c
Jorge Docampo <jorge.docampo@outlook.com>
2023-07-18 14:49:37 +0000

    first commit

---
+++ etlframework/etlframework/transformations.py
@@ -0,0 +1,10 @@
+import pyspark.sql.functions as F
+
+
+def transform_data(input_df):
+    ''' This function takes a dataframe as input and returns a transformed
+    dataframe'''
+    transformed_df = (input_df
+                      .groupBy('Location',)
+                      .agg(F.sum('ItemCount').alias('TotalItemCount')))
+    return transformed_df
